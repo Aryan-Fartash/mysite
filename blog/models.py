b7a4efd3bd0113b4d97c720d6019f613928df4a3
Aryan-Fartash <fartasharyan@gmail.com>
2024-09-16 21:13:10 -0600

    making more tables
    making more tables on database

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Post(models.Model):
     title=models.CharField(max_length=225)
     content=models.TextField()
+    counted_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    published_date=models.DateField(null=True)
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=225)
+    subject=models.CharField(max_length=225)
+    email=models.EmailField()
+    message=models.TextField()
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
